2628af64923b7dbae4101f3ac9a9e7b20c518717
Jan Berdajs <mrbrdo@gmail.com>
2013-01-24 18:35:52 +0100

    use nil by default

--- lib/nullobject_chain.rb
+++ lib/nullobject_chain.rb
@@ -1,18 +1,27 @@
 class NullChainer < BasicObject
+  class << self
+    attr_accessor :use_nullobject
+  end
+  @use_nullobject = false
+
   def initialize(obj)
     @obj = obj
   end
 
   def get
-    @obj
+    if @obj.is_a?(::NilClass) && ::NullChainer.use_nullobject
+      ::Null::Object.instance
+    else
+      @obj
+    end
   end
 
   def method_missing(name, *args, &block)
     begin
-      ::NullChainer.new(@obj.send(name, *args, &block)) 
+      ::NullChainer.new(@obj.send(name, *args, &block))
     rescue ::NoMethodError
       if @obj.nil?
-        ::NullChainer.new(::Null::Object.instance)
+        ::NullChainer.new(nil)
       else
         super
       end
